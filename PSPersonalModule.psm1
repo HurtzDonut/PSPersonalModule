ef353828eb56f2bf4f756fafab09de0f6efa1e21
Allen <jallen@bankatfirst.com>
2019-05-08 10:21:02 -0400

    Removed commented lines for '$PrivatePath' variables
    Added '$Private' files to 'ForEach' loop

--- PSPersonalModule.psm1
+++ PSPersonalModule.psm1
@@ -1,14 +1,16 @@
 $PublicPath     = Join-Path -Path $PSScriptRoot -ChildPath Public
-# $PrivatePath    = Join-Path -Path $PSScriptRoot -ChildPath Private
+$PrivatePath    = Join-Path -Path $PSScriptRoot -ChildPath Private
 
 $PublicFiles    = Get-ChildItem -Path $PublicPath -Include *.ps1 -Recurse
-# $PrivateFiles   = Get-ChildItem -Path $PrivatePath -Include *.ps1 -Recurse
+$PrivateFiles   = Get-ChildItem -Path $PrivatePath -Include *.ps1 -Recurse
 
 
-ForEach ($File in $PublicFiles) {
+ForEach ($File in ($PublicFiles + $PrivateFiles)) {
     Try {
         Import-Module -Path $File.FullName -ErrorAction Stop
     } Catch {
         Write-Warning ('Failed to load : {0}' -F $File.BaseName)
     }
 }
+
+Export-ModuleMember -Function $PublicFiles.BaseName
